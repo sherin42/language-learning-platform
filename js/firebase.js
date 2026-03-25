84328f28ec36ce6be470c6ce445ca014def5b9a4
Sherin Mariam John <sherinjohn4223@gmail.com>
2026-03-26 02:59:00 +0530

    Moved API key to config.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { firebaseConfig } from "../config.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCH8AnbR1Tu91iDGTUIJeW_jzY4l5zwvDM",
-  authDomain: "language-learning-platfo-fa50f.firebaseapp.com",
-  projectId: "language-learning-platfo-fa50f",
-  storageBucket: "language-learning-platfo-fa50f.firebasestorage.app",
-  messagingSenderId: "440392156554",
-  appId: "1:440392156554:web:2fb0507db69de6c721f473"
-};
 
 /* ✅ IMPORTANT: EXPORT app */
 export const app = initializeApp(firebaseConfig);
